32c86a17902d9422fa83b770d433808dbdf32316
Fatma Aydoğdu <fatmaaydogdu581@gmail.com>
2021-01-11 21:28:20 +0300

    fetchdata

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Basket,Favorites,Product} from './pages';
+import {Basket,Favorites,Products} from './pages';
 
 const Tab = createBottomTabNavigator();
 function Router() {
@@ -13,7 +13,7 @@ function Router() {
     <Tab.Navigator>
       <Tab.Screen name="Basket Screen" component={Basket}/>
       <Tab.Screen name="Favorites Screen" component={Favorites}/>
-      <Tab.Screen name="Product Screen" component={Product}/>
+      <Tab.Screen name="Product Screen" component={Products}/>
     </Tab.Navigator>
   </NavigationContainer>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState,useEffect} from 'react';
+import { View, Text } from 'react-native';
+import {axios} from 'axios';
+
+
+const api_url = 'https://fakestoreapi.com/products';
+const [productList, setproductList] = useState([]);
+
+
 
 export  function Products() {
+
+
+    function fetchdata(){
+        console.log();
+        axios.get(api_url).then((response) => setproductList(response.data))
+    }
+
+    useEffect(() => {
+    fetchdata()
+    }, [])
+
     return (
         <View>
-            <Text>Product</Text>
+            <Text>{productList.title}</Text>
         </View>
     )
 }
+
