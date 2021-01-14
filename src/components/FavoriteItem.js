aea3e7187f3ac570928d5693ba3ebbfbc4b8e188
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-14 23:41:14 +0300

    favorite item style

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function FavoriteItem({item}) {
   return (
-    <View>
-      <Text>{item.title}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.price}>{item.price}</Text>
+      <Text style={styles.description}>{item.description}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    elevation: 10,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  price: {},
+  description: {},
+});
 
 export {FavoriteItem};
