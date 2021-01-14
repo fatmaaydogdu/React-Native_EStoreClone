1066729fdfb00d5ba1fb3b0f686318ea3421e96c
Fatma Aydoğdu <fatmaaydogdu581@gmail.com>
2021-01-14 23:53:59 +0300

    favorite styles

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image,Dimensions} from 'react-native';
 
 function FavoriteItem({item}) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.price}>{item.price}</Text>
+      <View style={styles.imageContainer}>
+        <Image 
+      style={styles.image}
+      source={{uri: item.image}}
+      resizeMode={'center'}
+      />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.price}>{item.price} ₺</Text>
+      </View>
+      
+      </View>
+      
       <Text style={styles.description}>{item.description}</Text>
     </View>
   );
@@ -14,15 +25,37 @@ function FavoriteItem({item}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    margin: 5,
+    padding: 5,
     elevation: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  imageContainer:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+  },
+  titleContainer:{
+    alignContent:'flex-end',
+  },
+  image: {
+    width: Dimensions.get('window').width * 0.2,
+    height: Dimensions.get('window').height * 0.2,
   },
   title: {
     fontSize: 15,
     fontWeight: 'bold',
   },
-  price: {},
-  description: {},
+  price: {
+    color:'red',
+    fontWeight:'bold',
+  },
+  description: {
+    textAlign:'justify',
+    padding:5,
+    
+  },
 });
 
 export {FavoriteItem};
