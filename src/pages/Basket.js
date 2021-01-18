ea8bff4ac082e0502057f3675af45f1ce9a28498
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-01-18 22:58:25 +0300

    'basket's modal'

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  Button,
+} from 'react-native';
 import {useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
 import {BasketItem} from '../components/BasketItem';
 import HeaderItem from '../components/HeaderItem';
 import Empty from '../components/Empty';
+import Modal from 'react-native-modal';
 
 function Basket() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   const dispatch = useDispatch();
   const basketList = useSelector((state) => state.basket);
   console.log(basketList);
@@ -24,7 +40,7 @@ function Basket() {
   );
 
   return (
-    <View>
+    <View style={{flex: 1}}>
       <FlatList
         ListEmptyComponent={<Empty />}
         ListHeaderComponent={<HeaderItem header="SEPET" />}
@@ -32,8 +48,80 @@ function Basket() {
         data={basketList}
         renderItem={renderBasket}
       />
+      <View>
+        <TouchableOpacity style={modalStyle.btn} onPress={toggleModal}>
+          <Text style={modalStyle.btnTxt}>Sipariş geçmişi</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Modal style={modalStyle.container} isVisible={isModalVisible}>
+        <View style={{flex: 1}}>
+          <Image
+            style={modalStyle.image}
+            // source={{uri: item.image}}
+            resizeMode={'center'}
+          />
+          <View style={modalStyle.textContainer}>
+            {/* <Text style={modalStyle.title}>{item.title}</Text>
+            <Text style={modalStyle.price}>{item.price} ₺</Text> */}
+          </View>
+          <TouchableOpacity style={modalStyle.btn} onPress={toggleModal}>
+            <Text style={modalStyle.btnTxt}>KAPAT</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const modalStyle = StyleSheet.create({
+  btn: {
+    backgroundColor: '#f57f17',
+    padding: 5,
+    margin: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: Dimensions.get('window').width / 3,
+
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  btnTxt: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+    padding: 5,
+  },
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    height: Dimensions.get('window').height / 2,
+  },
+  image: {
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height / 3,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  title: {
+    flex: 2,
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'left',
+    color: '#333',
+  },
+  price: {
+    flex: 1,
+    textAlign: 'right',
+    fontFamily: 'monospace',
+    fontSize: 20,
+    color: 'red',
+    fontWeight: 'bold',
+  },
+});
 
 export {Basket};
